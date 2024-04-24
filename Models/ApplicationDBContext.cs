1ad43315ecae86e4431c2c3984138ccdc0a63a99
ShaneKanishka98 <shanekanishka1998@gmail.com>
2024-04-24 18:22:06 +0530

    My First App using ReadWrite and Entity Framework Controllers

---
+++ Models/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace coreFirstCRUDApplication.Models
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-75O0631G;Database=CRUDAppDB;Integrated Security=true;TrustServerCertificate=true;");
+        }
+    }
+}
